269a6fc99247c542dd25c90c74f8abfdea428f4a
Vladim72m <vladim72dx@tutamail.com>
2025-11-11 21:19:04 +0300

    titanic.py

--- titanic.py
+++ titanic.py
@@ -1,1 +1,12 @@
-gggg
+import matplotlib.pyplot as plt
+import pandas as pd 
+data = pd.read_parquet('titanic.parquet')
+data.to_csv('titanic.csv', index=False)  # конвертация в csv
+table = data.groupby(['Pclass', 'Survived']).size().unstack(fill_value=0) # группируем данные для анализа
+table_procent = table.div(table.sum(axis=1), axis=0) * 100 # перевод в проценты
+table_procent.plot(kind='bar', stacked=True)  # делается столбчатая диаграмма в процентах 
+plt.title('Выживаемость пассажиров Титаника')   #Подписи вывод графика
+plt.xlabel('Класс билета')
+plt.ylabel('% людей')
+plt.legend(['Не выжил', 'Выжил'])
+plt.show()  # показываем  график
